ca85617e1fea73e2c439ad4b05a260a76e5e34ba
unknown <hashimsandhu490@gmail.com>
2024-06-25 14:12:21 -0700

    Initial commit

--- db.js
+++ db.js
@@ -1,33 +1,70 @@
+// const mongoose = require('mongoose');
+// require('dotenv').config();
+// // Define MongoDB connection URL
+
+//  //const mongoUrl = process.env.MONGODB_URL_LOCAL;
+//  const mongoUrl = process.env.MONGODB_URL;
+
+// // SetUp MongoDB Connection
+
+// // mongoose.connect(mongoUrl,{
+ 
+// // })
+
+// mongoose.connect(mongoUrl, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     tls: true,  // Use `tls` instead of `ssl`
+//     tlsAllowInvalidCertificates: false  // Set to `false` to ensure certificates are validated
+//   });
+  
+
+// // Mongoose maintain connections 
+
+// const db = mongoose.connection;
+
+// // Define Event Listners for database connection
+
+// db.on('connected', ()=>{
+//     console.log('Connected to MongoDB server');
+// })
+
+// db.on('error', (error)=>{
+//     console.log(' MongoDB connection error:',error);
+// })
+
+// db.on('disconnected', ()=>{
+//     console.log('MongoDB disconnected');
+// })
+
+// // Export the database connection
+// module.exports = db;
+
 const mongoose = require('mongoose');
 require('dotenv').config();
-// Define MongoDB connection URL
 
-// const mongoUrl = process.env.MONGODB_URL_LOCAL;
 const mongoUrl = process.env.MONGODB_URL;
 
-// SetUp MongoDB Connection
+mongoose.connect(mongoUrl, {
 
-mongoose.connect(mongoUrl,{
- 
-})
-
-// Mongoose maintain connections 
+  ssl: true,
+  tlsAllowInvalidCertificates: true  // Use this cautiously
+});
 
 const db = mongoose.connection;
 
-// Define Event Listners for database connection
+db.on('connected', () => {
+  console.log('Connected to MongoDB server');
+});
 
-db.on('connected', ()=>{
-    console.log('Connected to MongoDB server');
-})
+db.on('error', (error) => {
+  console.log('MongoDB connection error:', error);
+});
 
-db.on('error', (error)=>{
-    console.log(' MongoDB connection error:',error);
-})
+db.on('disconnected', () => {
+  console.log('MongoDB disconnected');
+});
 
-db.on('disconnected', ()=>{
-    console.log('MongoDB disconnected');
-})
-
-// Export the database connection
 module.exports = db;
+
+
